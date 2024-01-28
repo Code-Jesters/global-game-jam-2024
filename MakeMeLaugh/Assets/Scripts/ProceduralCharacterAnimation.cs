40e24282f8d1402394c82cd366901a5c8910875f
Dan McCue <daniel.mccue@woven-planet.global>
2024-01-28 00:54:23 -0800

    Implemented a very rough proof-of-concept player character IK demo

--- MakeMeLaugh/Assets/Scripts/ProceduralCharacterAnimation.cs
+++ MakeMeLaugh/Assets/Scripts/ProceduralCharacterAnimation.cs
@@ -2,5 +2,62 @@ using UnityEngine;
 
 public class ProceduralCharacterAnimation : MonoBehaviour
 {
-    //
+    public Transform objToPickUp;
+    Animator animator;
+
+    public Transform Left_UpperLeg;
+    public Transform Head;
+    public float Weight = 1.0f;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        //var target = transform.Find("Skeleton/Hips/Left_UpperLeg/Left_LowerLeg/Left_Foot");
+        Left_UpperLeg = transform.Find("Skeleton/Hips/Left_UpperLeg");
+        Head = transform.Find("Skeleton/Hips/Spine/Chest/UpperChest/Neck/Head");
+        objToPickUp = Head;
+
+        Debug.Log("=== DJMC ProceduralCharacterAnimation ===");
+        for (var i = 0; i < animator.parameterCount; ++i)
+        {
+            var parameter = animator.GetParameter(i);
+            string valueStr = "";
+            if (parameter.type == AnimatorControllerParameterType.Float)
+            {
+                valueStr = "" + animator.GetFloat(parameter.name);
+            }
+            else if (parameter.type == AnimatorControllerParameterType.Int)
+            {
+                //valueStr = "" + animator.GetInt(parameter.name);
+                valueStr = "<TBD>";
+            }
+            else if (parameter.type == AnimatorControllerParameterType.Bool)
+            {
+                valueStr = "" + animator.GetBool(parameter.name);
+            }
+            else if (parameter.type == AnimatorControllerParameterType.Trigger)
+            {
+                //valueStr = "" + animator.GetTrigger(parameter.name);
+                valueStr = "<TBD>";
+            }
+            Debug.Log("  " + parameter.name + "<" + parameter.type.ToString() + ">: " + valueStr);
+        }
+    }
+
+    void Update()
+    {
+        //animator.enabled = false;
+    }
+
+    void OnAnimatorIK(int layerIndex)
+    {
+        //Debug.Log("OnAnimatorIK(" + layerIndex + ")");
+        //float reach = animator.GetFloat("RightHandReach");
+        float reach = Weight;
+        animator.SetIKPositionWeight(AvatarIKGoal.RightHand, reach);
+        if (objToPickUp != null)
+        {
+            animator.SetIKPosition(AvatarIKGoal.RightHand, objToPickUp.position);
+        }
+    }
 }
