39818509291dc9354ff5e40544ddd8e29645e877
zpallin <zpallin@gmail.com>
2024-01-28 19:31:00 -0500

    Colors assigned to networked users by emote choice

--- MakeMeLaugh/Assets/PlayerDisplay.cs
+++ MakeMeLaugh/Assets/PlayerDisplay.cs
@@ -1,19 +1,33 @@
 using LobbyRelaySample;
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Netcode;
 using Unity.Services.Lobbies;
 using UnityEngine;
 
-public class PlayerDisplay : MonoBehaviour
+public class PlayerDisplay : NetworkBehaviour
 {
     [SerializeField]
     private GameObject body;
+    NetworkVariable<int> emote = new NetworkVariable<int>();
+
     // Start is called before the first frame update
     void Start()
     {
+        emote.OnValueChanged += ApplyEmoteColor;
+        if (this.IsLocalPlayer)
+        {
+            emote.Value = (int)GameManager.Instance.m_LocalUser.Emote.Value;
+        }
+    }
+
+    private void ApplyEmoteColor(int previousValue, int newValue)
+    {
         Material[] materials = body.GetComponent<Renderer>().materials;
         Color color = Color.white;
-        switch (GameManager.Instance.m_LocalUser.Emote.Value)
+        EmoteType _emote = (EmoteType) newValue;
+        switch (_emote)
         {
             case EmoteType.Tongue:
                 color = Color.blue;
@@ -38,6 +52,5 @@ public class PlayerDisplay : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
     }
 }
